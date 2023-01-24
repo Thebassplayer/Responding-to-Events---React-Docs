a634f3205841fbc3d194379e40dd310c6e28dbc4
CodeSandbox <support@codesandbox.io>
2023-01-24 12:19:13 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+/**
+ * Responding to Events
+ *
+ * https://beta.reactjs.org/learn/responding-to-events
+ */
+
+import "./App.css";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
