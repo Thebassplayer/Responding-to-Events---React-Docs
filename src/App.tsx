f0f82eae2b03826ee181157e63579d6313552454
Roy Lopez <windup.dues-0f@icloud.com>
2023-02-27 14:25:56 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 /**
  * Responding to Events
  *
@@ -6,8 +8,64 @@
 
 import "./App.css";
 
+/**
+ * Button
+ */
+
+type buttonProps = {
+  message: string;
+  children: string;
+};
+
+function Button({ message, children }: buttonProps) {
+  const handleClick = (e: { stopPropagation: () => void }) => {
+    e.stopPropagation();
+    alert(message);
+  };
+
+  return <button onClick={handleClick}>{children}</button>;
+}
+
+/**
+ * Nav container
+ */
+
+type navProps = {
+  children: React.ReactNode;
+};
+
+const navStyles = {
+  backgroundColor: "lightgray",
+  padding: "1rem",
+  border: "1px solid Blue",
+  display: "flex",
+  gap: "1rem",
+} as React.CSSProperties;
+
+function Nav({ children }: navProps): JSX.Element {
+  return (
+    <div>
+      <div style={navStyles} onClick={() => alert("Nav clicked")}>
+        <h1>Nav</h1>
+        {children}
+      </div>
+    </div>
+  );
+}
+
+/**
+ * App
+ */
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Nav>
+        <Button message="Hello, world!">Click me!</Button>
+        <Button message="Hello, guachita!">Hola guacha!</Button>
+      </Nav>
+    </div>
+  );
 }
 
 export default App;
